eec9e0aeb31e977bf89ef06511d170e694f38630
Joel <jp2049@york.ac.uk>
2023-02-18 17:58:51 +0000

    Implemented basic checking that correct names are assigned to the correct textures

--- tests/src/cs/eng1/tests/AssetTests.java
+++ tests/src/cs/eng1/tests/AssetTests.java
@@ -4,10 +4,21 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+
+import java.util.HashMap;
+
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+
+import cs.eng1.piazzapanic.food.FoodTextureManager;
 
 @RunWith(GdxTestRunner.class)
 public class AssetTests {
+
+
+
+
+
 
     @Test
     /**
@@ -20,12 +31,14 @@ public class AssetTests {
     @Test
     public void cookedPattyAssetTest(){
         // Checks if "cooked_patty.png" exists
-        assertTrue("Passes if cooked_patty.png", Gdx.files.internal("food/original/cooked_patty.png").exists());
+        assertTrue("Passes if cooked_patty.png exists", Gdx.files.internal("food/original/cooked_patty.png").exists());
     }
+    
     @Test
     public void uncookedPattyAssetTest(){
         assertTrue("Passes if food/original/uncooked_patty.png exists", Gdx.files.internal("food/original/uncooked_patty.png").exists());
     }
+
     @Test
     public void lettuceAssetTest(){
         assertTrue("Passes if food/glitch/vegetable/lettuce.png exists", Gdx.files.internal("food/glitch/vegetable/lettuce.png").exists());
@@ -60,5 +73,23 @@ public class AssetTests {
     public void saladAssetTest(){
         assertTrue("Passes if food/glitch/misc/salad.png exists", Gdx.files.internal("food/glitch/misc/salad.png").exists());
     }
+
+    @Test
+    public void badlogicTest(){
+        assertTrue("Passes if badlogic.jpg exists", Gdx.files.internal("badlogic.jpg").exists());
+    }
+
+    @Test
+    public void testingAssetsAreCorrectlyMade(){
+        // This checks the foodType fetches the correct texture in FoodTextureManager
+        FoodTextureManager foodTextureManager = new FoodTextureManager();
+        Texture pattyTexture = foodTextureManager.getTexture("patty_cooked");
+        Texture truePattyTexture = new Texture(Gdx.files.internal("food/original/cooked_patty.png"));
+    
+        assertEquals("The foodType matches the texture returned", pattyTexture.getTextureData().toString(), truePattyTexture.getTextureData().toString());
+    }
 }
+
+
+
     
