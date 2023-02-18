0a97cd9a42730710fd03bf18a308494e2c174d32
Joel <jp2049@york.ac.uk>
2023-02-18 16:07:19 +0000

    Asset tests for FoodTextureManager

--- tests/src/cs/eng1/tests/AssetTests.java
+++ tests/src/cs/eng1/tests/AssetTests.java
@@ -18,9 +18,47 @@ public class AssetTests {
     }
 
     @Test
-    public void bucketAssetTest() {
-        // System.out.println(Gdx.files.internal("bucket.png"));
-        assertTrue("Passes if test asset is loaded", Gdx.files.internal("badlogic.jpg").exists());
+    public void cookedPattyAssetTest(){
+        // Checks if "cooked_patty.png" exists
+        assertTrue("Passes if cooked_patty.png", Gdx.files.internal("food/original/cooked_patty.png").exists());
+    }
+    @Test
+    public void uncookedPattyAssetTest(){
+        assertTrue("Passes if food/original/uncooked_patty.png exists", Gdx.files.internal("food/original/uncooked_patty.png").exists());
+    }
+    @Test
+    public void lettuceAssetTest(){
+        assertTrue("Passes if food/glitch/vegetable/lettuce.png exists", Gdx.files.internal("food/glitch/vegetable/lettuce.png").exists());
     }
 
+    @Test
+    public void lettuceChoppedAssetTest(){
+        assertTrue("Passes if food/original/lettuce_chopped.png exists", Gdx.files.internal("food/original/lettuce_chopped.png").exists());
+    }
+
+    @Test
+    public void tomatoAssetTest(){
+        assertTrue("Passes if food/glitch/fruit/tomato.png exists", Gdx.files.internal("food/glitch/fruit/tomato.png").exists());
+    }
+        
+    @Test
+    public void tomatoChoppedAssetTest(){
+        assertTrue("Passes if food/original/tomato_chopped.png exists", Gdx.files.internal("food/original/tomato_chopped.png").exists());
+    }
+        
+    @Test
+    public void bunAssetTest(){
+        assertTrue("Passes if food/glitch/misc/bun.png exists", Gdx.files.internal("food/glitch/misc/bun.png").exists());
+    }
+        
+    @Test
+    public void burgerAssetTest(){
+        assertTrue("Passes if food/glitch/misc/sandwich_burger_04.png exists", Gdx.files.internal("food/glitch/misc/sandwich_burger_04.png").exists());
+    }
+        
+    @Test
+    public void saladAssetTest(){
+        assertTrue("Passes if food/glitch/misc/salad.png exists", Gdx.files.internal("food/glitch/misc/salad.png").exists());
+    }
 }
+    
