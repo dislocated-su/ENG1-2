54c3cd1205cd5274fda845774721473ac430ad40
Joel <jp2049@york.ac.uk>
2023-02-24 13:44:59 -0800

    Tested FixedStack (no fringe cases), edit FixedStack to make maxSize public

---
+++ tests/src/cs/eng1/tests/FixedStackTests.java
@@ -0,0 +1,47 @@
+package cs.eng1.tests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+import cs.eng1.piazzapanic.chef.FixedStack;
+
+@RunWith(GdxTestRunner.class)
+public class FixedStackTests {
+    FixedStack<Integer> fixedStack = new FixedStack<Integer>(5);
+    @Test
+    public void fixedStackLengthTest() {
+        assertEquals("The stack has a max size equal to its size.",fixedStack.maxSize, 5);
+    }
+
+    @Test
+    public void hasSpaceTest(){
+        FixedStack<Integer> fixedStack = new FixedStack<Integer>(1);
+        assertTrue("hasSpace is true when the stack isn't full",fixedStack.hasSpace());
+    }
+
+    @Test
+    public void hasNoSpaceTest(){
+        FixedStack<Integer> fixedStack = new FixedStack<Integer>(0);
+        assertFalse("hasSpace is false when at max size",fixedStack.hasSpace());
+    }
+
+    @Test
+    public void pushWithSpaceTest(){
+        FixedStack<Integer> fixedStack = new FixedStack<Integer>(5);
+        for(int i = 1; i<5; i++){
+            fixedStack.push(1);
+            assertEquals("Push should work when the size is less than maxSize", i, fixedStack.size());
+        }
+    }
+
+    @Test
+    public void pushWithoutSpaceTest(){
+        FixedStack<Integer> fixedStack = new FixedStack<Integer>(0);
+        fixedStack.push(1);
+        assertEquals("Push should not work when the size is equal to maxSize",0, fixedStack.size());
+    }
+
+    //Couldn't test fringe cases of stacks being larger than maxSize
+}
