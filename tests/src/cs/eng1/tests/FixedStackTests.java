94238ff2f69c30e3f7b29db16e00a41e8a58b7a4
Andrey Samoilov <40268316+dislocated-su@users.noreply.github.com>
2023-02-26 18:14:36 +0000

    add more asset tests
    Co-authored-by: roh518 <roh518@users.noreply.github.com>
    Co-authored-by: Joel <Skuxa@users.noreply.github.com>

--- tests/src/cs/eng1/tests/FixedStackTests.java
+++ tests/src/cs/eng1/tests/FixedStackTests.java
@@ -15,35 +15,38 @@ import cs.eng1.piazzapanic.food.FoodTextureManager;
 @RunWith(GdxTestRunner.class)
 public class FixedStackTests {
     FixedStack<Integer> fixedStack = new FixedStack<Integer>(5);
-    //Tests if the stack's max size is properly assigned
+
+    // Tests if the stack's max size is properly assigned
     @Test
     public void fixedStackLengthTest() {
         assertEquals("The stack has a max size equal to its size.", 5, fixedStack.maxSize);
     }
-    //Tests for hasSpace function being able to see space
+
+    // Tests for hasSpace function being able to see space
     @Test
-    public void hasSpaceTest(){
+    public void hasSpaceTest() {
         FixedStack<Integer> fixedStack = new FixedStack<Integer>(1);
         assertTrue("hasSpace is true when the stack isn't full", fixedStack.hasSpace());
     }
 
     @Test
-    public void hasNoSpaceTest(){
+    public void hasNoSpaceTest() {
         FixedStack<Integer> fixedStack = new FixedStack<Integer>(0);
         assertFalse("hasSpace is false when at max size", fixedStack.hasSpace());
     }
-    //Tests push for a range of values towars the maximum size
+
+    // Tests push for a range of values towars the maximum size
     @Test
-    public void pushWithSpaceTest(){
+    public void pushWithSpaceTest() {
         FixedStack<Integer> fixedStack = new FixedStack<Integer>(5);
-        for(int i = 1; i<6; i++){
+        for (int i = 1; i < 6; i++) {
             fixedStack.push(1);
             assertEquals("Push should work when the size is less than maxSize", i, fixedStack.size());
         }
     }
 
     @Test
-    public void pushWithoutSpaceTest(){
+    public void pushWithoutSpaceTest() {
         FixedStack<Integer> fixedStack = new FixedStack<Integer>(0);
         fixedStack.push(1);
         assertEquals("Push should not work when the size is equal to maxSize", 0, fixedStack.size());
