e14c290d5214291b6822ec8493de07a15dd7d0a8
Thomas Pauline <tp1025@york.ac.uk>
2023-03-24 18:04:42 +0000

    started stationCollider tests

---
+++ tests/src/cs/eng1/tests/StationColliderTests.java
@@ -0,0 +1,36 @@
+package cs.eng1.tests;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import cs.eng1.piazzapanic.chef.Chef;
+import cs.eng1.piazzapanic.chef.ChefManager;
+import cs.eng1.piazzapanic.stations.IngredientStation;
+import cs.eng1.piazzapanic.stations.StationCollider;
+import cs.eng1.piazzapanic.ui.StationUIController;
+
+@RunWith(GdxTestRunner.class)
+
+public class StationColliderTests{
+    ChefManager chefManager = new ChefManager(0, null, null);
+    Chef chef = new Chef(null, null, chefManager);
+    private StationUIController uiController = mock(StationUIController.class);
+    IngredientStation station = new IngredientStation(0, null, uiController, null, null);
+
+    
+    @Test
+    /**
+     * Tests that notfiyObservers and getLastNotification work correctly
+     */
+    public void testNotifyObservers(){
+        StationCollider stationCollider = new StationCollider(chefManager);
+        stationCollider.register(station);
+        stationCollider.notifyObservers(chef);
+        station.update(null);
+        assertEquals("tests that notfiyObservers tells the observers(stations) about a chef" ,chef, station.nearbyChef);
+        assertEquals("tests getLastNotification returns the last thing that was notified" ,chef, stationCollider.getLastNotification());
+    }
+}
