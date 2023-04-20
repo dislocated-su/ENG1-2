0064a2efd18607b20973793ebd988feefda1d385
Joel <jp2049@york.ac.uk>
2023-04-20 05:31:49 -0700

    more tests

--- tests/src/cs/eng1/tests/ChefManagerTests.java
+++ tests/src/cs/eng1/tests/ChefManagerTests.java
@@ -23,7 +23,7 @@ import com.badlogic.gdx.physics.box2d.World;
 
 @RunWith(GdxTestRunner.class)
 public class ChefManagerTests {
-    World world = new World(new Vector2(0, 0), true);
+     World world = new World(new Vector2(0, 0), true);
     UIOverlay overlay = mock(UIOverlay.class);
     KeyboardInput kbInput = new KeyboardInput();
     ChefManager chefManager = new ChefManager(1, overlay, null, kbInput);
@@ -32,6 +32,32 @@ public class ChefManagerTests {
     public void actTest() {
         kbInput.keyDown(Keys.E);
         chefManager.act(1);
-        assertEquals(chefManager.getCurrentChef(), chefManager.getChefs());
+        int chefIndex = chefManager.getChefs().indexOf(chefManager.getCurrentChef());
+        assertEquals(
+            "After E is pressed, the current chef should be initialised.", 
+            chefManager.getCurrentChef(), 
+            chefManager.getChefs().get(chefIndex));
+        kbInput.keyDown(Keys.E);
+        chefManager.act(1);
+        assertEquals(
+            "Pressing E after having a current chef should change the current chef.", 
+            chefManager.getCurrentChef(), 
+            chefManager.getChefs().get(chefIndex+1));
+        kbInput.keyDown(Keys.E);
+        chefManager.act(1);
+        assertEquals(
+            "When E is pressed and currentchef is the end of the array, it should wrap to the first chef.", 
+            chefManager.getCurrentChef(), 
+            chefManager.getChefs().get(chefIndex));
     }
+
+    @Test
+    public void setCurrentChefTests() {
+        assertNull(chefManager.getCurrentChef());
+        for (i = 0; i <= chefManager.getChefs().length; i++) {
+            chefManager.setCurrentChef(chefManager.getChefs().get(i));
+            assertEquals(chefManager.getChefs().get(i), chefManager.getCurrentChef());
+        }
+    }
+
 }
