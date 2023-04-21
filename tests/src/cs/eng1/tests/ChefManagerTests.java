acd85262c91778701b1555a2c553fcc8700550c6
Joel <118197925+Skuxa@users.noreply.github.com>
2023-04-21 14:12:03 +0000

    fixed chefmanagertests

--- tests/src/cs/eng1/tests/ChefManagerTests.java
+++ tests/src/cs/eng1/tests/ChefManagerTests.java
@@ -3,18 +3,9 @@ package cs.eng1.tests;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 
-import cs.eng1.piazzapanic.chef.Chef;
 import cs.eng1.piazzapanic.chef.ChefManager;
-import cs.eng1.piazzapanic.food.FoodTextureManager;
-import cs.eng1.piazzapanic.food.ingredients.Lettuce;
-import cs.eng1.piazzapanic.food.ingredients.Patty;
-import cs.eng1.piazzapanic.stations.ChoppingStation;
-import cs.eng1.piazzapanic.stations.StationAction;
-import cs.eng1.piazzapanic.stations.StationAction.ActionType;
-import cs.eng1.piazzapanic.ui.StationUIController;
 import cs.eng1.piazzapanic.ui.UIOverlay;
 import cs.eng1.piazzapanic.utility.KeyboardInput;
-import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import com.badlogic.gdx.Input.Keys;
@@ -23,10 +14,10 @@ import com.badlogic.gdx.physics.box2d.World;
 
 @RunWith(GdxTestRunner.class)
 public class ChefManagerTests {
-     World world = new World(new Vector2(0, 0), true);
+    World world = new World(new Vector2(0, 0), true);
     UIOverlay overlay = mock(UIOverlay.class);
     KeyboardInput kbInput = new KeyboardInput();
-    ChefManager chefManager = new ChefManager(1, overlay, null, kbInput);
+    ChefManager chefManager = new ChefManager(1, overlay, world, kbInput);
 
     @Test
     public void actTest() {
@@ -54,14 +45,13 @@ public class ChefManagerTests {
     @Test
     public void setCurrentChefTests() {
         assertNull(chefManager.getCurrentChef());
-        for (i = 0; i <= chefManager.getChefs().length+1; i++) {
+        for (int i = 0; i < chefManager.getChefs().size(); i++) {
             chefManager.setCurrentChef(chefManager.getChefs().get(i));
             assertEquals(
                 "setCurrentChef should properly assign currentChef.", 
                 chefManager.getChefs().get(i), 
                 chefManager.getCurrentChef());
         }
-
     }
 
 }
