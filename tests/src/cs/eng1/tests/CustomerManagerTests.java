6b35322edb558cda7a4f6da50da556e361e09a05
Joel <118197925+Skuxa@users.noreply.github.com>
2023-04-26 00:20:11 +0100

    Chef Tests (#80)
    * started on fixed tests
    
    * getBody() method added
    
    * Further into testing, less impacted by momentum
    
    * more coverage
    
    * grab item tests
    
    * remove popIngredient usage
    
    ---------
    
    Co-authored-by: Skuxa <Skuxa@users.noreply.github.com>

--- tests/src/cs/eng1/tests/CustomerManagerTests.java
+++ tests/src/cs/eng1/tests/CustomerManagerTests.java
@@ -1,3 +1,34 @@
 package cs.eng1.tests;
 
-public class CustomerManagerTests {}
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import cs.eng1.piazzapanic.chef.ChefManager;
+import cs.eng1.piazzapanic.customer.CustomerManager;
+import cs.eng1.piazzapanic.food.FoodTextureManager;
+import cs.eng1.piazzapanic.food.recipes.Pizza;
+import cs.eng1.piazzapanic.food.recipes.Recipe;
+import cs.eng1.piazzapanic.ui.UIOverlay;
+import cs.eng1.piazzapanic.utility.KeyboardInput;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(GdxTestRunner.class)
+public class CustomerManagerTests {
+
+    UIOverlay overlay = mock(UIOverlay.class);
+    CustomerManager customerManager = new CustomerManager(overlay, 5, 0);
+    FoodTextureManager textureManager = new FoodTextureManager();
+
+    @Test
+    public void initTests() {
+        customerManager.init(textureManager);
+        assertEquals(
+            new Pizza(textureManager).getTexture(),
+            customerManager.getFirstOrder().getTexture()
+        );
+    }
+}
