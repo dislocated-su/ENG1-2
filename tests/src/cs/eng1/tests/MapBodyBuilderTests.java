c004d97853fff13d7f9c152e21d028cd1ec05566
Joel <118197925+Skuxa@users.noreply.github.com>
2023-04-28 14:27:23 +0000

    make file

---
+++ tests/src/cs/eng1/tests/MapBodyBuilderTests.java
@@ -0,0 +1,17 @@
+package cs.eng1.tests;
+
+import static org.junit.Assert.*;
+
+import cs.eng1.piazzapanic.box2d.MapBodyBuilder;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.TextureData;
+import com.badlogic.gdx.graphics.glutils.FileTextureData;
+import cs.eng1.piazzapanic.chef.FixedStack;
+import cs.eng1.piazzapanic.food.FoodTextureManager;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(GdxTestRunner.class)
+public class MapBodyBuilderTests {
+    MapBodyBuilder bodyBuilder = new MapBodyBuilder();
+}
