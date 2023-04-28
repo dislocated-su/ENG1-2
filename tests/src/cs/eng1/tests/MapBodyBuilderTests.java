93d0e1319bf60fe679cfa7a94588ba20ef6514e4
Joel <118197925+Skuxa@users.noreply.github.com>
2023-04-28 16:47:21 +0000

    added test-map, started tests

--- tests/src/cs/eng1/tests/MapBodyBuilderTests.java
+++ tests/src/cs/eng1/tests/MapBodyBuilderTests.java
@@ -6,12 +6,26 @@ import cs.eng1.piazzapanic.box2d.MapBodyBuilder;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.TextureData;
 import com.badlogic.gdx.graphics.glutils.FileTextureData;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
+
 import cs.eng1.piazzapanic.chef.FixedStack;
 import cs.eng1.piazzapanic.food.FoodTextureManager;
+import cs.eng1.piazzapanic.utility.MapLoader;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(GdxTestRunner.class)
 public class MapBodyBuilderTests {
-    MapBodyBuilder bodyBuilder = new MapBodyBuilder();
+    MapLoader mapLoader = new MapLoader("test-map.tmx");
+    World world = new World(new Vector2(0, 0), true);
+    MapLayer layer = mapLoader.getMap().getLayers().get("Obstacles");
+    @Test
+    public void buildShapesTest() {
+        Array bodies = MapBodyBuilder.buildShapes(layer, 16, world);
+        assertEquals("", bodies.get(1).getClass().toString());
+    }
 }
