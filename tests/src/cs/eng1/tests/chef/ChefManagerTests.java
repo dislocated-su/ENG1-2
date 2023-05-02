ae30269680e9e845842301e64c2ecb06115cf796
Joel <118197925+Skuxa@users.noreply.github.com>
2023-05-02 19:01:02 +0000

    started / edited chefmanagertests

--- tests/src/cs/eng1/tests/chef/ChefManagerTests.java
+++ tests/src/cs/eng1/tests/chef/ChefManagerTests.java
@@ -1,98 +1,168 @@
-// package cs.eng1.tests.chef;
+package cs.eng1.tests.chef;
 
-// import static org.junit.Assert.*;
-// import static org.mockito.Mockito.mock;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
-// import com.badlogic.gdx.Input.Keys;
-// import com.badlogic.gdx.math.Vector2;
-// import com.badlogic.gdx.physics.box2d.World;
-// import cs.eng1.piazzapanic.chef.ChefManager;
-// import cs.eng1.piazzapanic.ui.UIOverlay;
-// import cs.eng1.piazzapanic.utility.KeyboardInput;
-// import cs.eng1.tests.GdxTestRunner;
-// import org.junit.Ignore;
-// import org.junit.Test;
-// import org.junit.runner.RunWith;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
-// @Ignore
-// @RunWith(GdxTestRunner.class)
-// public class ChefManagerTests {
+import cs.eng1.piazzapanic.chef.Chef;
+import cs.eng1.piazzapanic.chef.ChefManager;
+import cs.eng1.piazzapanic.customer.CustomerManager;
+import cs.eng1.piazzapanic.food.FoodTextureManager;
+import cs.eng1.piazzapanic.ui.StationUIController;
+import cs.eng1.piazzapanic.ui.UIOverlay;
+import cs.eng1.piazzapanic.utility.KeyboardInput;
+import cs.eng1.piazzapanic.utility.MapLoader;
+import cs.eng1.tests.GdxTestRunner;
 
-//     World world = new World(Vector2.Zero, true);
-//     UIOverlay overlay = mock(UIOverlay.class);
-//     KeyboardInput kbInput = new KeyboardInput();
-//     ChefManager chefManager = new ChefManager(1, overlay, world, kbInput);
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
-//     @Test
-//     public void initialiseTests() {
-//         chefManager.init();
-//         assertFalse(
-//             "There should be at least 2 chefs.",
-//             chefManager.getChefs().size() < 2
-//         );
-//         for (int i = 0; i < chefManager.getChefs().size(); i++) {
-//             assertNotEquals(
-//                 "The texture for each chef should exist.",
-//                 "badlogic.jpg",
-//                 chefManager.getChefs().get(i).getTexture().toString()
-//             );
-//             assertEquals(
-//                 "ChefManager init should set the X of each chef to initial values.",
-//                 chefManager.getChefs().get(i).getX(),
-//                 chefManager.getChefX()[i],
-//                 0.1
-//             );
-//             assertEquals(
-//                 "ChefManager init should set the Y of each chef to initial values.",
-//                 chefManager.getChefs().get(i).getY(),
-//                 chefManager.getChefY()[i],
-//                 0.1
-//             );
-//             assertFalse(
-//                 "Input should not be allowed at the start of a game instance.",
-//                 chefManager.getChefs().get(i).isInputEnabled()
-//             );
-//         }
-//     }
+@RunWith(GdxTestRunner.class)
+public class ChefManagerTests {
 
-//     @Test
-//     public void actTests() {
-//         kbInput.keyDown(Keys.E);
-//         chefManager.act(1);
-//         int chefIndex = chefManager
-//             .getChefs()
-//             .indexOf(chefManager.getCurrentChef());
-//         assertEquals(
-//             "After E is pressed, the current chef should be initialised.",
-//             chefManager.getCurrentChef(),
-//             chefManager.getChefs().get(chefIndex)
-//         );
-//         kbInput.keyDown(Keys.E);
-//         chefManager.act(1);
-//         assertEquals(
-//             "Pressing E after having a current chef should change the current chef.",
-//             chefManager.getCurrentChef(),
-//             chefManager.getChefs().get(chefIndex + 1)
-//         );
-//         kbInput.keyDown(Keys.E);
-//         chefManager.act(1);
-//         assertEquals(
-//             "When E is pressed and currentchef is the end of the array, it should wrap to the first chef.",
-//             chefManager.getCurrentChef(),
-//             chefManager.getChefs().get(chefIndex)
-//         );
-//     }
+    World world = new World(Vector2.Zero, true);
+    UIOverlay overlay = mock(UIOverlay.class);
+    KeyboardInput kbInput = new KeyboardInput();
+    ChefManager chefManager = new ChefManager(1, overlay, world, kbInput);
+    Stage stage = mock(Stage.class);
+    FoodTextureManager textureManager = new FoodTextureManager();
+    
+    StationUIController stationUIController = new StationUIController(
+        stage,
+        null
+    );
+    CustomerManager customerManager = new CustomerManager(
+        1,
+        overlay,
+        world,
+        5,
+        0
+    );
 
-//     @Test
-//     public void setCurrentChefTests() {
-//         assertNull(chefManager.getCurrentChef());
-//         for (int i = 0; i < chefManager.getChefs().size(); i++) {
-//             chefManager.setCurrentChef(chefManager.getChefs().get(i));
-//             assertEquals(
-//                 "setCurrentChef should properly assign currentChef.",
-//                 chefManager.getChefs().get(i),
-//                 chefManager.getCurrentChef()
-//             );
-//         }
-//     }
-// }
+    Texture fake = new Texture(
+        Gdx.files.internal(
+            "badlogic.jpg"
+        )
+    );
+    Chef fakeChef = new Chef(fake, Vector2.Zero, chefManager);
+
+    MapLoader mapLoader = new MapLoader("test-map.tmx");
+
+    @Test
+    public void startTests() {
+        assertEquals(
+            "The game should start with 2 chefs.", 
+            2, 
+            chefManager.getChefs().size()
+        );
+        for (Chef chef : chefManager.getChefs()) {
+            assertNotEquals(
+                "The chef textures should not be badlogic.", 
+                "badlogic.jpg", 
+                chef.getTexture().toString()
+            );
+            assertFalse(
+                "Chefs should not take inputs by default.", 
+                chef.isInputEnabled()
+            );
+        }
+    }
+
+    @Test
+    public void initTests() {
+        mapLoader.loadWaypoints(
+            "Waypoints",
+            "cookspawnid",
+            "aispawnid",
+            "lightid",
+            "aiobjective"
+        );
+        mapLoader.createStations(
+            "Stations",
+            "Sensors",
+            chefManager,
+            stage,
+            stationUIController,
+            textureManager,
+            customerManager
+        );
+        chefManager.init(mapLoader.cookSpawnpoints);
+        int i = 0;
+        for (Chef chef : chefManager.getChefs()) {
+            assertTrue (
+                mapLoader.cookSpawnpoints.get(i)
+                    .epsilonEquals(chef.getBody().getPosition(),
+                    1f
+                    )
+                );
+            i++;
+        }
+    }
+
+    @Test
+    public void hireChefTests() {
+        mapLoader.loadWaypoints(
+            "Waypoints",
+            "cookspawnid",
+            "aispawnid",
+            "lightid",
+            "aiobjective"
+        );
+        mapLoader.createStations(
+            "Stations",
+            "Sensors",
+            chefManager,
+            stage,
+            stationUIController,
+            textureManager,
+            customerManager
+        );
+        for (int i = 2; i < 4; i++) {
+            chefManager.hireChef(
+                mapLoader.cookSpawnpoints.get(i - 2), 
+                stage
+            );
+            assertEquals(i + 1, chefManager.getChefs().size());
+
+            assertFalse(
+                "Chefs should not take inputs by default after being hired.", 
+                chefManager.getChefs().get(i).isInputEnabled()
+            );
+        }
+         
+    }
+
+    @Test
+    public void actTests() {
+        kbInput.keyDown(Keys.E);
+        chefManager.act(1);
+        int chefIndex = chefManager
+            .getChefs()
+            .indexOf(chefManager.getCurrentChef());
+        assertEquals(
+            "After E is pressed, the current chef should be initialised.",
+            chefManager.getCurrentChef(),
+            chefManager.getChefs().get(chefIndex)
+        );
+        kbInput.keyDown(Keys.E);
+        chefManager.act(1);
+        assertEquals(
+            "Pressing E after having a current chef should change the current chef.",
+            chefManager.getCurrentChef(),
+            chefManager.getChefs().get(chefIndex + 1)
+        );
+        kbInput.keyDown(Keys.E);
+        chefManager.act(1);
+        assertEquals(
+            "When E is pressed and currentchef is the end of the array, it should wrap to the first chef.",
+            chefManager.getCurrentChef(),
+            chefManager.getChefs().get(chefIndex)
+        );
+    }
+}
